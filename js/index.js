5e587bcfda1d72ae3c382d4ed8d2a9e34782bf9e
bento123saban <82221414+bento123saban@users.noreply.github.com>
2021-07-09 22:45:37 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
+window.addEventListener('scroll', function(e) {
+	// showHideNav()
+})
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-65px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// // navbar script 
+// let prevScrollpos = window.pageYOffset;
+// // show and hide navbar
+// function showHideNav() {
+// 	let currentScrollPos = window.pageYOffset;
+// 	if (prevScrollpos > currentScrollPos) {
+// 		document.getElementById("navbar").style.top = "0";
+// 	} else {
+// 		document.getElementById("navbar").style.top = "-65px";
+// 	}
+// 	prevScrollpos = currentScrollPos;
+// }
