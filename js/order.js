c199a877f4d9b9fdd8973146c41aa01ba3d51734
bento123saban <82221414+bento123saban@users.noreply.github.com>
2021-07-09 22:45:01 +0900

    Create order.js

---
+++ js/order.js
@@ -0,0 +1,340 @@
+
+//data buket
+const buketData = [
+    {
+        ID :  "01001",
+        tipe : "Buket Tipe 1",
+        nama : "Biskuit Oreo",
+        img : "buket.oreo.jpg",
+        deskripsi : "Buket Oreo, dengan biskuit Oreo dibungkus kertas biru dan pita merah serta hiasan berbentuk bunga putih. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    },
+    {
+        ID :  "01002",
+        tipe : "Buket Tipe 2",
+        nama : "Snack Beng-Beng",
+        img : "buket.beng-beng.jpg",
+        deskripsi : "Buket Snack Beng-Beng & Top, dibungkus kertas hitam corak garis putih, dengan snack Beng-beng 2 rasa (coklat & strawbery), snack Top rasa coklat dan hiasan biskuit better diikat pita merah. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    },
+    {
+        ID :  "01003",
+        tipe : "Buket Tipe 3",
+        nama : "Biskuit Better",
+        img : "buket.better.jpg",
+        deskripsi : "Buket Biskuit Better, dengan biskuit Oreo dibungkus kertas putih dan pita merah. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    },
+    {
+        ID :  "01004",
+        tipe: "Buket Tipe 4",
+        nama : "Bunga Mawar",
+        img : "buket.mawar.jpg",
+        deskripsi : "Buket Bunga Mawar, dengan Bunga Mawar Merah dan Putih dibungkus kertas corak ungu, gold dan p ink, dengan hiasan pita silver. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    },
+    {
+        ID :  "01005",
+        tipe: "Buket Tipe 5",
+        nama : "Uang Rp 5000, 40 Lembar",
+        img : "buket.uang5.jpg",
+        deskripsi : "Buket Uang Rp 5000 Asli/Mainan #40 Lembar, dibungkus ketas pink dan coklat serta hiasan bunga merah dan pita silver merah. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    },
+    {
+        ID :  "01006",
+        tipe : "Buket Tipe 6",
+        nama : "Coklat SilverQueen",
+        img : "buket.silverqueen.jpg",
+        deskripsi : "Buket Coklat SilverQueen, dengan isian SilverQueen Chocolate Bar, dibungkus kertas hitam pink dan putih, serta hiasan bunga merah dan pita merah. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    },
+    {
+        ID :  "01007",
+        tipe : "Buket Tipe 7",
+        nama : "Snack Wafle",
+        img : "buket.wafle.jpg",
+        deskripsi : "Buket Snack Wafle, dengan isian snack Wafle dan biskuit Arden dibungkus kertas Hitam dengan corak gold dan merah serta dihias bunga merah dan pita merah. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    },
+    {
+        ID :  "01008",
+        tipe : "Buket Tipe 8",
+        nama : "Bunga Flanel",
+        img : "buket.flanel.jpg",
+        deskripsi : "Buket Bunga Flanel, dengan bunga flanel pink dan biru cyan, dinungkus kertas silver dan gold dan dihias pita silver. Isi dapat disesuaikan (harga dapat berubah tergantung isi)",
+        harga : 50000
+    }
+]
+
+const ritelData = [
+    {
+        ID :  "02001",
+        tipe : '',
+        nama : "Tas 01",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02002",
+        tipe : '',
+        nama : "Tas 02",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02003",
+        tipe : '',
+        nama : "Tas 03",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02004",
+        tipe : '',
+        nama : "Tas 04",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02005",
+        tipe : '',
+        nama : "Tas 05",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02006",
+        tipe : '',
+        nama : "Tas 06",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02007",
+        tipe : '',
+        nama : "Tas 07",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02008",
+        tipe : '',
+        nama : "Tas 08",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02009",
+        tipe : '',
+        nama : "Tas 09",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    },
+    {
+        ID :  "02010",
+        tipe : '',
+        nama : "Tas 10",
+        img : "buket.wafle.jpg",
+        deskripsi : "",
+        harga : 123
+    }
+]
+
+
+function perulanganData(array)
+{
+    let html =''
+    array.map(item => {
+        html += 
+        `
+            <div class="card" data-nama="${item.tipe} ${item.nama}">
+                <img src="asset/buket/${item.img}" alt="" class="img">
+                <div class="text">
+                    <p class="nama">${item.tipe}<br>${item.nama}</p>
+                    <p class="desc">${item.deskripsi}</p>
+                    <p class="harga"><span class='rp'>~Rp ${item.harga}*</span> <span data-id="${item.ID}" class="tambah" >Order</span></p>
+                </div>
+            </div>
+        `
+    })
+    return html
+}
+
+//memuat data buket di HTML
+document.querySelector('.buket-box').innerHTML = perulanganData(buketData)
+
+//memuat data ritel di HTML
+document.querySelector('.ritel-box').innerHTML = perulanganData(ritelData)
+
+// Order Script
+
+function daftarOrder() // menyusun daftar item yang telah diorder
+{
+    let daftar = '' // menampung hasil dari daftar order
+    if (orderArray.length == 0 ) // jika orderArray kosong, panjang = 0, maka
+    { 
+        daftar = 
+        `
+            <p class="no-item">Belum ada item yang diorder</p> 
+        ` // daftar order kosong
+    }
+    else// tapi, jika daftar order tidak kosong, maka
+    { 
+        orderArray.forEach(item => { // lakukan perulangan unutk setiap item dalam daftar order
+            let html = `
+                <div class="order-item" data-id="${item.ID}">
+                    <li>${item.nama}</li>
+                    <input type="number" class="jumlah-item" value="${item.jumlah}" required>
+                    <i class="fas fa-trash"></i>
+                </div>
+            ` // kemudian masukan data dalam HTML untuk menjadi tampilan seperti kartu
+            daftar += html
+        })
+    }
+    return document.querySelector('.order-pilih').innerHTML = daftar // mengembalikan hasil dari daftar order menjadi HTML dari kelas order-pilih
+}
+
+function hitungTotal() // menghitung total bayar
+{
+    let total = 0 // varibal menampung hasil hitung total bayar
+    orderArray.map(item => { // perulangan untuk setiap item dalam daftar order
+        total += parseFloat(item.harga) * parseFloat(item.jumlah) // mengalikan harga dari item 
+    });
+    document.querySelector('.total-harga').innerHTML = 'Total Harga : Rp. ' + total // mengembalikan/menampilkan hasil hitung pada element total harga
+}
+
+// mendapatkan data dari item dalam array, dengan parameter id sebagai id yang akan dicari dan parameter array sebagai array tempat mengambil data
+function temukanItem(id, array) {
+    const i = array.findIndex( item => item.ID == id)
+    return array[i]
+}
+
+function tambahOrder(element) {
+    let ID = element.getAttribute('data-id') //mengambil ID item
+    let parameter = 0 //parameter ; jika 0 beluk ditambahkan, jika 1 sudah ditambahkan
+    orderArray.forEach( index => { //perulangan untuk mengecek item sudah ditambahkan atau belum
+        if (index.ID == ID) {
+            parameter = 1 // jika sudah ditambahakan parameter menjadi (1)
+            return ''// hentikan proses
+        }
+    }) //jika item belum ditambahkan (param = 0), ambil data item dara variabel simpan data (buketData atau ritelData)
+    if(parameter == 0) //jika item belum ditambahkan (param = 0), ambil data item dara variabel simpan data (buketData atau ritelData)
+    {
+        let  item = {} // variabel Objek untuk menampung data dari item yang ditambhakan
+        if (ID[0] == 0 && ID[1] == 1) // jika angka pertama ID = 0 dan angka kedua = 1, item adalah buket
+        { 
+            item = temukanItem(ID, buketData) // jika item adalah buket, masukan ID item dan array buketData untuk menemukan item(buket) dari dalam data/array buketData, kemudian data dari item tersebut ditampung dalam variabel item(tipe objek '{}') diatas
+        } 
+        else if (ID[0] == 0 && ID[1] == 2)// jika angka pertama ID = 0 dan angka kedua = 1, item adalah buket
+        {
+            item = temukanItem(ID, ritelData)
+        }
+        const objek =
+        {
+            ID : item.ID,
+            nama : item.tipe + " - " + item.nama,
+            harga : item.harga,
+            jumlah : 1
+        }
+        orderArray.push(objek)
+    }
+}
+
+function hapusItem(element){
+    const ID = element.parentElement.getAttribute('data-id')
+    let i = orderArray.findIndex( item => item.ID == ID)
+    orderArray.splice(i, 1)
+}
+
+function gantiNilai(element){
+    console.log(element)
+    const ID = element.parentElement.getAttribute('data-id')
+    console.log(ID)
+    let i = orderArray.findIndex(index => index.ID == ID)
+    let value = parseFloat(element.value)
+    if (value > 0) {
+        orderArray[i].jumlah = value
+    } else {
+        element.value = ''
+        orderArray[i].jumlah = 0
+    }
+    console.info(orderArray[i].jumlah)
+    hitungTotal()
+}
+
+function jikaKosong(element) {
+    if (element.value == '') {
+        element.style.borderColor = 'red'
+    } else {
+        element.style.borderColor = ''
+    }
+}
+
+function cekIsi() {
+    let parameter = 0
+    document.querySelectorAll('.order-input').forEach(function (item) {
+        if (item.value == '') {
+            parameter = 1
+            item.style.borderColor = 'red'
+        }
+    })
+
+    if (orderArray.length == 0) {
+        parameter = 1
+    }
+    if(parameter == 0){
+        let konfirmasi = confirm('Anda akan dialihkan ke WhatsApp. Segera kirim text pesan yang tertera untuk mengkonfirmasi orderan anda !')
+        if (konfirmasi) {
+            order()
+        }
+    }
+}
+
+function order(){
+    const nama = document.querySelector('#order-nama').value
+    const telp = document.querySelector('#order-telpon').value
+    const deskripsi = document.querySelector('#order-deskripsi').value
+    console.log(nama, telp, deskripsi)
+
+    let bayar = new Array()
+    document.getElementsByName('bayar').forEach( check => {
+        if (check.checked) {
+            console.log('check')
+            if (check.value == 'cod') {
+                console.log('cod')
+                const alamat = document.querySelector('#alamat').value
+                bayar = ['COD', `Alamat : ${alamat}`]
+            } else if (check.value == 'ambil') {
+                console.log('ambil')
+                bayar = ['Ambil Sendiri', `Lokasi kami : mmnbvcxzzxcvbnmloopokiujuyhgtrfdewsaq`]
+            }
+        }
+    })
+    
+    let orderan = ''
+    orderArray.forEach(function(item){
+        orderan += `${item.nama} (${item.jumlah}).\n`
+    })
+    let total = 0 // varibal menampung hasil hitung total bayar
+    orderArray.map(item => { // perulangan untuk setiap item dalam daftar order
+        total += parseFloat(item.harga) * parseFloat(item.jumlah) // mengalikan harga dari item 
+    });
+ 
+    let dataWA = `Order Baru !! - ${new Date()}\n\n${nama} - ${telp}\n${orderan} Total : Rp. ${total}\n\nDeskripsi : ${deskripsi}\n\nPembayaran :\n${bayar[0]}, ${bayar[1]}
+    `
+    const encode = encodeURI(dataWA)
+    const linkWA = `https://wa.me/+6281354741823?text=${encode}`
+    window.open(linkWA)
+    console.info(linkWA)
+    console.info(dataWA)
+}
